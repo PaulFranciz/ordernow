2b663b24a39a59d7951717c3f64e29c4fda321ed
Otu Emmanuella <otumanuella@gmail.com>
2025-03-05 10:37:24 +0100

    rectified netlify d eploy issues 2

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -15,9 +15,7 @@ export default function MarketingLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <MainNav />
-      <main className="flex-1">
-        {children}
-      </main>
+      {children}
     </div>
   );
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/marketing/hero";
 export default function MarketingPage() {
   return (
-    <main>
+    <main className="flex-1">
       <Hero />
       {/* Your marketing page content */}
     </main>
